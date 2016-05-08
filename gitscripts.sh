05864395de7cc2c4edbccc5ac46c823e5ce100df
Daniel Loeffelholz <dloeffel@thoughtworks.com>
2016-05-08 03:00:24 +0200

    %|dl|added init scripts and fixed some other stuff

--- gitscripts.sh
+++ gitscripts.sh
@@ -1,91 +1,110 @@
 #!/bin/bash
-if [[ -z "$PROJECT_ROOT" ]]
-then
-  echo "Using projectroot$PROJECT_ROOT"
-else 
-	PROJECT_ROOT="$PWD"
-	echo "Using current dir: $PROJECT_ROOT"
-fi
-	
-	
-PROJECT_ROOT="$PWD"
+source ./commit.sh
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+addToBashRC(){
+	# TODO: extend it to more bash specific files
+	printf '%s' 'Do you want to append to source this script to your bashrc? (y/n)'
+	read  yn
+	if [ "$yn" = "y" ]; then
+		if [ -f ~/.bashrc ]; then
+			echo "source $SCRIPT_DIR/gitscripts.sh" >>~/.bashrc	
+		else 
+			echo ".bashrc does not exist"
+		fi
+	else
+		break;
+	fi
+}
+
+initZshRC(){
+	printf '%s' 'Do you want to append to source this script to your .zshrc? (y/n)'
+	read  yn
+	if [ "$yn" = "y" ]; then
+		if [ -f "~/.zshrc" ]; then
+			echo 'source $SCRIPT_DIR/gitscripts.sh' >>~/.zshrc
+		else 
+			echo ".zshrc does not exist"
+		fi
+	else
+		break;
+	fi
+}
+
 
 stashall()  {
 	(
-	cd $PROJECT_ROOT 
-	find . -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} stash \;
+	if [ -d "./.git" ]
+	then
+		echo "$PWD is git root, stashing locally"
+		git stash
+	else
+		echo "$PWD is no git root, stashing subfolders"
+		find . -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} stash \;
+	fi
+		echo 'Done.'
 	)
 }
 
 pullall()  {
 	(
-	cd $PROJECT_ROOT 
-	find . -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} pull --rebase \;
+	if [ -d "./.git" ]
+	then
+		echo "$PWD is git root, pulling locally"
+		git pull --rebase
+	else
+		echo "$PWD is no git root, pulling subfolders"
+		find . -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} pull --rebase \;
+	fi
+	echo 'Done.'
 	)
 }
 
-showchanges(){
+statusall(){
 	(
-	cd $PROJECT_ROOT 
-	find . -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} status \;
+	if [ -d "./.git" ]
+	then
+		echo "$PWD is git root, getting status of current project"
+		git status
+	else
+		echo "$PWD is no git root, getting status of subfolders"
+		find . -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} status \;
+	fi
+		echo 'Done.'
 	)
 }
 
-launchAtStartZSH(){
-	echo $SCRIPT_DIR
-	# tmp="$pwd"
-	# goto user dir
-	# check for .zshrc
-	# if not exists, break or create
-	# if exists, append line: 
-	# export PROJECT_ROOT=tmp
-	# source $SCRIPT_DIR/functions.sh &> /dev/null
+upto ()
+{
+    if [ -z "$1" ]; then
+        return
+    fi
+    local upto=$1
+    cd "${PWD/\/$upto\/*//$upto}"
 }
 
-launchAtStartBASHRC(){
-	# tmp="$pwd"
-	# goto user dir
-	# check for .bashrc 
-	# .bash_profile
-	# if not exists, break or create
-	# if exists, append line: 
-	# export PROJECT_ROOT=tmp
-	# source $SCRIPT_DIR/functions.sh &> /dev/null
+_upto_bash()
+{
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    local d=${PWD//\//\ }
+    COMPREPLY=( $( compgen -W "$d" -- "$cur" ) )
 }
 
-gadd(){
-	echo "addit"
+#TODO: autocomplete with zsh not working yet
+_upto_zsh()
+{
+    local word completions
+    word="$1"
+    completions=${PWD//\//\ }
+	echo completions
+    reply=${completions[@]}
 }
 
+#auto complete for zsh
+if [ "$SHELL" = "/bin/zsh" ]; then
+	compctl -K _upto_zsh upto
+else #for all other shells
+	complete -F _upto_bash upto
+fi
 
-gcommit(){
-	echo "commitit"
-}
 
-projectHooks(){
-	echo 'hooks'
-}
-
-pushitgood(){
-	git status;
-	while true; do
-		printf '%s ' 'Do you wish to add and commit the remaining changes? yn'
-	    read -q yn
-	    case $yn in
-	        [Yy]* ) gadd(); break;;
-			[Nn]* ) break;;
-	        * ) echo "Please answer y or n.";;
-	    esac
-	done
-}
-
-export -f stashall
-export -f pullall
-export -f showchanges
-export -f launchAtStartZSH
-export -f launchAtStartBASHRC
-export -f gadd
-export -f gcommit
-export -f projectHooks
-export -f pushitgood
